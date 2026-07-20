11176ea151daa919e1e6d2968b871c870447bd03
David <eyitayobembe@gmail.com>
2026-07-20 18:27:49 +0100

    refactor: clean up legacy routing and obsolete components

--- app/(dashboards)/reviewer/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardShell } from "@/components/dashboard/DashboardShell";
-
-export default function ReviewerLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DashboardShell role="reviewer">{children}</DashboardShell>;
-}
--- app/(dashboards)/user/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardShell } from "@/components/dashboard/DashboardShell";
-
-export default function UserLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DashboardShell role="contributor">{children}</DashboardShell>;
-}
